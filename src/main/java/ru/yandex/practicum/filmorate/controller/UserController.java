a3b3b002d421cd4dd9af5861abd4cabe9fbdfe60
Елена <elbonreed05@yandex.ru>
2024-06-04 03:42:16 +0300

    add controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User add(@RequestBody @Valid User user) {
+        log.info("Запрос на добавление пользователя: {}", user);
+
+        String login = user.getLogin();
+        if (login.contains(" ")) {
+            log.error("Логин не может содержать пробелы");
+            throw new ValidationException("Логин не может содержать пробелы");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.debug("Имя пустое, использование логина для инициализации");
+            user.setName(login);
+        }
+        log.info("Валидация прошла успешно");
+
+        log.debug("Установка id");
+        user.setId(getNextId());
+
+        log.debug("Добавление пользователя");
+        users.put(user.getId(), user);
+
+        log.info("Пользователь успешно добавлен: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody @Valid User user) {
+        log.info("Запрос на обновление пользователя: {}", user);
+
+        log.trace("Проверка id");
+        if (users.get(user.getId()) == null) {
+            log.error("Пользователь с указанным айди не существует");
+            throw new NotFoundException("Пользователь с указанным id не существует");
+        }
+
+        String login = user.getLogin();
+        if (login.contains(" ")) {
+            log.error("Логин не может содержать пробелы");
+            throw new ValidationException("Логин не может содержать пробелы");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.debug("Имя == null, использование логина для инициализации");
+            user.setName(login);
+        }
+        log.info("Валидация прошла успешно");
+
+        log.debug("Обновление данных пользователя");
+        users.put(user.getId(), user);
+
+        log.info("Данные успешно обновлены: {}", user);
+        return user;
+    }
+
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,21 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class Film {
+    private int id;
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+    @Size(max = 200, message = "Длина описания не может превышать 200 символов")
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
 }
